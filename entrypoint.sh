2d287913c63d9058712e1acb2d3b6356a2dcb90f
Jose Galindo <jgalindo@tse.org.gt>
2025-06-23 21:31:42 -0600

    added entrypoint.sh static

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Recolectar archivos estáticos
+python manage.py collectstatic --noinput
+
 # Aplicar migraciones
 python manage.py migrate
 
-# Crear superusuario automáticamente (solo si no existe)
+# Crear superusuario si no existe
 echo "from django.contrib.auth import get_user_model; \
 User = get_user_model(); \
 User.objects.filter(username='admin').exists() or \
 User.objects.create_superuser('admin', 'admin@example.com', 'Admin1234')" \
 | python manage.py shell
 
-# Iniciar Gunicorn
+# Iniciar el servidor
 gunicorn config.wsgi:application
